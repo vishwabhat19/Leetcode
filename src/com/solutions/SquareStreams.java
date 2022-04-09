a05e91b1bb1c4332be29c3e3f6f870da284259b5
Vishwa <VB086617>
2022-04-09 14:13:04 +0530

    USing Streams to calculate Square of elements in a list

---
+++ src/com/solutions/SquareStreams.java
@@ -0,0 +1,16 @@
+package com.solutions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SquareStreams {
+
+    public static void main(String[] args) {
+        
+        List<String> list = Arrays.asList(new String[]{"1", "2", "3", "4"});
+        list = list.stream().map(s -> Integer.toString(Integer.parseInt(s) * Integer.parseInt(s))).collect(Collectors.toList());
+        list.stream().forEach(System.out::println);
+    }
+    
+}
