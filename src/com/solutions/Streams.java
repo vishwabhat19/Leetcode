53e406e2b30966f416bd0ff88b020ec0d876c53e
Vishwa <vishwa.bhat19@gmail.com>
2022-06-16 23:00:59 +0530

    Streams working out

--- src/com/solutions/Streams.java
+++ src/com/solutions/Streams.java
@@ -1,20 +1,73 @@
 package com.solutions;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /*
 Use Streams to output the frequency of each String
  */
+
+class Student {
+	Integer studentId;
+	String name;
+	Integer marks;
+
+	public Student(Integer studentId, String name, Integer marks)
+	{
+		this.studentId = studentId;
+		this.name = name;
+		this.marks = marks;
+	}
+
+	public Integer getStudentId()
+	{
+		return studentId;
+	}
+
+	public void setStudentId(Integer studentId)
+	{
+		this.studentId = studentId;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public Integer getMarks()
+	{
+		return marks;
+	}
+
+	public void setMarks(Integer marks)
+	{
+		this.marks = marks;
+	}
+}
 public class Streams
 {
 	public static void main(String[] args)
 	{
-		String s[] = {"batman", "superman", "batman", "batman"};
-		Map<String, Long> map = Arrays.stream(s).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-		map.entrySet().stream().forEach((e) -> System.out.println(e.getKey()+" "+e.getValue()));
+//		String s[] = {"batman", "superman", "batman", "batman"};
+//		Map<String, Long> map = Arrays.stream(s).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+//		map.entrySet().stream().forEach((e) -> System.out.println(e.getKey()+" "+e.getValue()));
+
+		Student s1 = new Student(1, "Krishna", 100);
+		Student s2 = new Student(2, "Rama", 96);
+		Student s3 = new Student(3, "Vishnu", 50);
+
+		List<Student> list = Arrays.asList(new Student[]{s1,s2,s3});
+		list.forEach(student -> System.out.println(student.getName()));
+
+		//Sort the students based on their marks.
+		List<Student> sortedList = list.stream().sorted(Comparator.comparing(Student::getMarks)).collect(Collectors.toList());
+		System.out.println("Sorted List by Marks");
+		sortedList.forEach(student -> System.out.println(student.getName()+" "+student.getMarks()));
 	}
 }
