76279815d515063ff322058f600850c2a30e0acf
Vishwa <vishwa.bhat19@gmail.com>
2022-07-07 12:15:39 +0530

    Changes

--- src/com/solutions/Lion.java
+++ src/com/solutions/Lion.java
@@ -1,2 +1,59 @@
-package com.solutions;public class Lion {
+package com.solutions;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class GoodFood{}
+class GoodWater{}
+public class Lion
+{
+
+	public void eatAndDrink(GoodFood food, GoodWater water) {
+		synchronized (food) {
+			System.out.println("Got Food!");
+			move();
+			synchronized (water) {
+				System.out.println("Got Water!");
+			}
+		}
+	}
+
+	public void drinkAndEat(GoodFood food, GoodWater water) {
+		synchronized (water) {
+			System.out.println("Got Water!");
+			move();
+			synchronized (food) {
+				System.out.println("Got Food");
+			}
+		}
+	}
+
+	public void move() {
+		try {
+			Thread.sleep(100);
+		}
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		Lion lion1 = new Lion();
+		Lion lion2 = new Lion();
+		GoodWater water = new GoodWater();
+		GoodFood food = new GoodFood();
+
+		ExecutorService service = null;
+		try
+		{
+			service = Executors.newScheduledThreadPool(10);
+			service.submit(() -> lion1.eatAndDrink(food,water));
+			service.submit(() -> lion2.drinkAndEat(food,water));
+		}
+		finally
+		{
+			if(service!=null) service.shutdown();
+		}
+	}
 }
