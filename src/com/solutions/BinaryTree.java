ee40c65a613490bb764cf6926e8b6cd9c7df4f11
Vishwa <vishwa.bhat19@gmail.com>
2022-07-11 23:27:20 +0530

    Changes

--- src/com/solutions/BinaryTree.java
+++ src/com/solutions/BinaryTree.java
@@ -7,6 +7,23 @@ public class BinaryTree
 	BinaryTree() {
 		root = null;
 	}
+
+	public boolean hasPathSum(TreeNode root, int targetSum) {
+		if(root == null) return false;
+
+		if(root.key == targetSum && root.left == null && root.right == null)
+			return true;
+
+		boolean ans = false;
+		int remainingSum = targetSum - root.key;
+
+		if(root.left!=null)
+			ans = ans || hasPathSum(root.left, remainingSum);
+		if(root.right!=null)
+			ans = ans || hasPathSum(root.right, remainingSum);
+
+		return ans;
+	}
 	public static void main(String[] args)
 	{
 		BinaryTree tree = new BinaryTree();
