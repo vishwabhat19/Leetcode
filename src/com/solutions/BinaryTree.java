b5be09f8579ad80b99f02a4e0893a0341ac7801c
Vishwa <vishwa.bhat19@gmail.com>
2022-07-17 01:51:10 +0530

    Function to calculate max depth of tree

--- src/com/solutions/BinaryTree.java
+++ src/com/solutions/BinaryTree.java
@@ -143,4 +143,16 @@ public class BinaryTree
 		}
 		return true;
 	}
+
+	static int maxDepthValue = Integer.MIN_VALUE;
+	static int currentDepth = 0;
+
+	static int maxDepth(TreeNode root) {
+		if(root == null) return 0;
+
+		int leftDepth = maxDepth(root.left);
+		int rightDepth = maxDepth(root.right);
+		if(leftDepth > rightDepth) return leftDepth + 1;
+		return rightDepth +1;
+	}
 }
