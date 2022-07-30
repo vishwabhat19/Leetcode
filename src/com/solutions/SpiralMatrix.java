23d253bf88746f39de8b4200bf823f63b0f17782
Vishwa <vishwa.bhat19@gmail.com>
2022-07-30 17:38:01 +0530

    832. Flipping an Image

--- src/com/solutions/SpiralMatrix.java
+++ src/com/solutions/SpiralMatrix.java
@@ -13,14 +13,36 @@ public class SpiralMatrix
 {
 	public static void main(String[] args)
 	{
-		int arr[][] = {{1,2,3},{4,5,6},{7,8,9}};
+		int arr[][] = { { 1,2,3 }, { 4,5,6}, {7,8,9}};
 		List<Integer> arrayList = spiralOrder(arr);
 		arrayList.forEach(System.out::println);
 	}
 
 	public static List<Integer> spiralOrder(int[][] matrix) {
 		List<Integer> list = new ArrayList<>();
-		Arrays.stream(matrix[0]).forEach(e -> list.add(e));
+		int left = 0;
+		int right = matrix[0].length - 1;
+		int top = 0;
+		int bottom = matrix.length - 1;
+		int size = (right+1) * (bottom+1);
+		while(list.size() != size) {
+			for(int i=left;i <= right && list.size()!=size; i++) {
+				list.add(matrix[top][i]);
+			}
+			top++;
+			for(int j=top;j<=bottom && list.size()!=size;j++) {
+				list.add(matrix[j][right]);
+			}
+			right--;
+			for(int i=right;i>=left && list.size()!=size;i--) {
+				list.add(matrix[bottom][i]);
+			}
+			bottom--;
+			for(int i=left;i<=right && list.size()!=size;i++)
+			{
+				list.add(matrix[bottom][i]);
+			}
+		}
 		return list;
 	}
 }
